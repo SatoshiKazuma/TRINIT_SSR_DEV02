4450f7e075912e2c26459bc37baddac2ee168a4e
SatoshiKazuma <sairamkumarm21@gmail.com>
2023-02-11 21:50:40 +0530

    Changed webdata from object to array of object, tackled "static reloading"

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
-window.webdata = {};
+window.webdata = [];
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    webdata[request.hostname] = request.size;
+	// webdata['hostname'] = request.hostname;
+	// webdata['size'] = request.size;
+	let hostnameIndex = webdata.findIndex(
+		(entry) => entry.hostname === request.hostname
+	);
+	if (hostnameIndex > -1) {
+		webdata[hostnameIndex].size += request.size;
+	} else {
+		webdata.push({ hostname: request.hostname, size: request.size });
+	}
+	console.log(webdata);
+	// webdata[request.hostname] = request.size;
 });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.create({ url: "popup.html" });
+	chrome.tabs.create({ url: "popup.html" });
 });
